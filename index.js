0c7a72bf5fcc59f67a144971021cf6c0f1196bdc
shady <shady2k@gmail.com>
2024-03-25 13:30:27 +0300

    Added swagger

--- index.js
+++ index.js
@@ -1,70 +1,123 @@
 const envToLogger = {
   development: {
-    level: 'debug',
+    level: "debug",
     transport: {
-      target: 'pino-pretty',
+      target: "pino-pretty",
       options: {
-        translateTime: 'HH:MM:ss Z',
-        ignore: 'pid,hostname',
+        translateTime: "HH:MM:ss Z",
+        ignore: "pid,hostname",
       },
     },
   },
   production: true,
   test: false,
-}
-
-const fastify = require('fastify')({
-  logger: envToLogger[process.env.NODE_ENV] ?? true // defaults to true if no entry matches in the map
-})
-const envFileName = `.env.${process.env.NODE_ENV || "development"}`;
-require("dotenv").config({ path: envFileName });
-const Sprut = require("./sprut.js");
-
-const sprut = new Sprut({
-  wsUrl: process.env.WS_URL,
-  sprutLogin: process.env.SPRUT_LOGIN,
-  sprutPassword: process.env.SPRUT_PASSWORD,
-  serial: process.env.SPRUT_SERIAL,
-  logger: fastify.log,
-});
-
-// Define the JSON Schema for the request body
-const updateSchema = {
-  body: {
-    type: "object",
-    properties: {
-      accessoryId: { type: "number" },
-      serviceId: { type: "number" },
-      characteristicId: { type: "number" },
-      value: { type: "boolean" },
-    },
-    required: ["accessoryId", "serviceId", "characteristicId", "value"],
-  },
 };
 
-fastify.post("/update", { schema: updateSchema }, async (request, reply) => {
-  try {
-    const result = await sprut.execute("update", request.body);
-    return {
-      ...request.body,
-      result
+const fastify = require("fastify")({
+  logger: envToLogger[process.env.NODE_ENV] ?? true, // defaults to true if no entry matches in the map
+});
+const envFileName = `.env.${process.env.NODE_ENV || "development"}`;
+require("dotenv").config({ path: envFileName });
+
+(async () => {
+  await fastify.register(require("@fastify/swagger"));
+
+  await fastify.register(require("@fastify/swagger-ui"), {
+    routePrefix: "/",
+    uiConfig: {
+      docExpansion: "full",
+      deepLinking: false,
+    },
+    uiHooks: {
+      onRequest: function (request, reply, next) {
+        next();
+      },
+      preHandler: function (request, reply, next) {
+        next();
+      },
+    },
+    staticCSP: true,
+    transformStaticCSP: (header) => header,
+    transformSpecification: (swaggerObject) => {
+      return swaggerObject;
+    },
+    transformSpecificationClone: true,
+  });
+
+  const Sprut = require("./sprut.js");
+  const sprut = new Sprut({
+    wsUrl: process.env.WS_URL,
+    sprutLogin: process.env.SPRUT_LOGIN,
+    sprutPassword: process.env.SPRUT_PASSWORD,
+    serial: process.env.SPRUT_SERIAL,
+    logger: fastify.log,
+  });
+
+  // Define the JSON Schema for the request body
+  const updateSchema = {
+    description: "Update an accessory",
+    tags: ["accessory"],
+    summary: "Updates the specified accessory",
+    body: {
+      type: "object",
+      properties: {
+        accessoryId: { type: "number" },
+        serviceId: { type: "number" },
+        characteristicId: { type: "number" },
+        value: { type: "boolean" },
+      },
+      required: ["accessoryId", "serviceId", "characteristicId", "value"],
+    },
+    response: {
+      200: {
+        description: "Successful response",
+        type: "object",
+        properties: {
+          accessoryId: { type: "number" },
+          serviceId: { type: "number" },
+          characteristicId: { type: "number" },
+          value: { type: "boolean" },
+          result: { type: "string" },
+        },
+      },
+    },
+  };
+
+  fastify.post("/update", { schema: updateSchema }, async (request, reply) => {
+    try {
+      const result = await sprut.execute("update", request.body);
+      return {
+        ...request.body,
+        result,
+      };
+    } catch (error) {
+      fastify.log.error(error);
+      reply
+        .code(500)
+        .send({ error: "An error occurred while processing your request." });
     }
- } catch (error) {
-    fastify.log.error(error);
-    reply.code(500).send({ error: 'An error occurred while processing your request.' });
- }
-});
+  });
 
-fastify.addHook("onClose", async () => {
-  await sprut.close();
-});
+  fastify.addHook("onClose", async () => {
+    await sprut.close();
+  });
 
-// Run the server!
-fastify.listen({ host: process.env.LISTENING_HOST || 'localhost', port: process.env.LISTENING_PORT || 3000 }, function (err, address) {
-  if (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-  // Server is now listening on ${address}
-  fastify.log.info(`Server listening on ${address}`);
-});
+  // Ensure all plugins are fully initialized before listening
+  await fastify.ready();
+
+  // Start the server
+  fastify.listen(
+    {
+      host: process.env.LISTENING_HOST || "localhost",
+      port: process.env.LISTENING_PORT || 3000,
+    },
+    function (err, address) {
+      if (err) {
+        fastify.log.error(err);
+        process.exit(1);
+      }
+      // Server is now listening on ${address}
+      fastify.log.info(`Server listening on ${address}`);
+    }
+  );
+})();
